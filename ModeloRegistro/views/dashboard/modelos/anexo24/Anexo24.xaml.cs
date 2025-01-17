08a300a6a28e8471e84ea063e458425c853ba54f
Roilan Lauzardo <125706938+rls1990@users.noreply.github.com>
2025-01-17 17:16:03 -0500

    views anexo4 nav

--- ModeloRegistro/views/dashboard/modelos/anexo24/Anexo24.xaml.cs
+++ ModeloRegistro/views/dashboard/modelos/anexo24/Anexo24.xaml.cs
@@ -21,12 +21,12 @@ namespace ModeloRegistro.views.dashboard.modelos.anexo24
 
         private void Button_Click_1( object sender,RoutedEventArgs e )
         {
-
+            PageService.toPage(new Upd());
         }
 
         private void Button_Click_2( object sender,RoutedEventArgs e )
         {
-            PageService.toPage(new Upd());
+            
         }
 
         private void Button_Click_3( object sender,RoutedEventArgs e )
