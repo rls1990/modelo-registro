8b443443f53fbec9beb7d301aab69a89242944a9
Roilan Lauzardo <125706938+rls1990@users.noreply.github.com>
2025-06-25 12:25:19 -0400

    quitar validaciones anexo20

--- ModeloRegistro/views/dashboard/modelos/anexo20/Add.xaml.cs
+++ ModeloRegistro/views/dashboard/modelos/anexo20/Add.xaml.cs
@@ -24,25 +24,25 @@ namespace ModeloRegistro.views.dashboard.modelos.anexo20
         {
             var anexo = new model.Anexo20_e()
             {
-                nombre_apellidos=nombre_apellidos.Text,
-                ciudadano=ciudadano.Text,
-                nombre_apellidos_menor_edad=nombre_apellidos_menor_edad.Text,
-                ciudadania_menor_edad=ciudadania_menor_edad.Text,
-                lugar_nacimiento_menor=lugar_nacimiento_menor.Text,
-                fecha_nacimiento_menor=fecha_nacimiento_menor.SelectedDate?.ToString("yyyy-MM-dd"),
-                pasaporte_menor=pasaporte_menor.Text,
-                padre=padre.Text,
-                ciudadania_padre=ciudadania_padre.Text,
-                madre=madre.Text,
-                ciudadania_madre=ciudadania_madre.Text,
-                ultima_direccion_padres=ultima_direccion_padres.Text,
-                nombre_persona_representante_legal=nombre_persona_representante_legal.Text,
-                recidir_cuba=recidir_cuba.IsChecked.ToString(),
-                recidir_en_exterior=recidir_en_exterior.IsChecked.ToString(),
-                razones_personales=razones_personales.Text,
-                nombre_funcionario_consular=nombre_funcionario_consular.Text,
-                telefono=telefono.Text,
-                email=email.Text
+                nombre_apellidos=ValidarCampo(nombre_apellidos.Text),
+                ciudadano=ValidarCampo(ciudadano.Text),
+                nombre_apellidos_menor_edad=ValidarCampo(nombre_apellidos_menor_edad.Text),
+                ciudadania_menor_edad=ValidarCampo(ciudadania_menor_edad.Text),
+                lugar_nacimiento_menor=ValidarCampo(lugar_nacimiento_menor.Text),
+                fecha_nacimiento_menor=ValidarCampo(fecha_nacimiento_menor.SelectedDate?.ToString("yyyy-MM-dd")),
+                pasaporte_menor=ValidarCampo(pasaporte_menor.Text),
+                padre=ValidarCampo(padre.Text),
+                ciudadania_padre=ValidarCampo(ciudadania_padre.Text),
+                madre=ValidarCampo(madre.Text),
+                ciudadania_madre=ValidarCampo(ciudadania_madre.Text),
+                ultima_direccion_padres=ValidarCampo(ultima_direccion_padres.Text),
+                nombre_persona_representante_legal=ValidarCampo(nombre_persona_representante_legal.Text),
+                recidir_cuba=ValidarCampo(recidir_cuba.IsChecked.ToString()),
+                recidir_en_exterior=ValidarCampo(recidir_en_exterior.IsChecked.ToString()),
+                razones_personales=ValidarCampo(razones_personales.Text),
+                nombre_funcionario_consular=ValidarCampo(nombre_funcionario_consular.Text),
+                telefono=ValidarCampo(telefono.Text),
+                email=ValidarCampo(email.Text)
             };
 
             if(ValidarAnexo(anexo))
@@ -65,32 +65,39 @@ namespace ModeloRegistro.views.dashboard.modelos.anexo20
             }
 
             // Verificar si cada campo es nulo o vacío
-            if(string.IsNullOrEmpty(model.nombre_apellidos)||
-                string.IsNullOrEmpty(model.ciudadano)||
-                string.IsNullOrEmpty(model.nombre_apellidos_menor_edad)||
-                string.IsNullOrEmpty(model.ciudadania_menor_edad)||
-                string.IsNullOrEmpty(model.lugar_nacimiento_menor)||
-                string.IsNullOrEmpty(model.fecha_nacimiento_menor)||
-                string.IsNullOrEmpty(model.pasaporte_menor)||
-                string.IsNullOrEmpty(model.padre)||
-                string.IsNullOrEmpty(model.ciudadania_padre)||
-                string.IsNullOrEmpty(model.madre)||
-                string.IsNullOrEmpty(model.ciudadania_madre)||
-                string.IsNullOrEmpty(model.ultima_direccion_padres)||
-                string.IsNullOrEmpty(model.nombre_persona_representante_legal)||
-                //string.IsNullOrEmpty(model.recidir_cuba)||
-                //string.IsNullOrEmpty(model.recidir_en_exterior)||
-                string.IsNullOrEmpty(model.razones_personales)||
-                string.IsNullOrEmpty(model.nombre_funcionario_consular)||
-                string.IsNullOrEmpty(model.telefono)||
-                string.IsNullOrEmpty(model.email))
-            {
-                return false; // Algún campo es nulo o vacío
-            }
+            //if(string.IsNullOrEmpty(model.nombre_apellidos)||
+            //    string.IsNullOrEmpty(model.ciudadano)||
+            //    string.IsNullOrEmpty(model.nombre_apellidos_menor_edad)||
+            //    string.IsNullOrEmpty(model.ciudadania_menor_edad)||
+            //    string.IsNullOrEmpty(model.lugar_nacimiento_menor)||
+            //    string.IsNullOrEmpty(model.fecha_nacimiento_menor)||
+            //    string.IsNullOrEmpty(model.pasaporte_menor)||
+            //    string.IsNullOrEmpty(model.padre)||
+            //    string.IsNullOrEmpty(model.ciudadania_padre)||
+            //    string.IsNullOrEmpty(model.madre)||
+            //    string.IsNullOrEmpty(model.ciudadania_madre)||
+            //    string.IsNullOrEmpty(model.ultima_direccion_padres)||
+            //    string.IsNullOrEmpty(model.nombre_persona_representante_legal)||
+            //    //string.IsNullOrEmpty(model.recidir_cuba)||
+            //    //string.IsNullOrEmpty(model.recidir_en_exterior)||
+            //    string.IsNullOrEmpty(model.razones_personales)||
+            //    string.IsNullOrEmpty(model.nombre_funcionario_consular)||
+            //    string.IsNullOrEmpty(model.telefono)||
+            //    string.IsNullOrEmpty(model.email))
+            //{
+            //    return false; // Algún campo es nulo o vacío
+            //}
 
             // Si todas las validaciones pasan
             return true; // La entidad Anexo20_e es válida
         }
 
+        private string ValidarCampo( string val )
+        {
+            string res = "";
+            if(!string.IsNullOrWhiteSpace(val))
+                res=val;
+            return res;
+        }
     }
 }
