96daf8216aa2a59608e8f16811b3b21f9761b333
Roilan Lauzardo <125706938+rls1990@users.noreply.github.com>
2025-06-25 11:31:32 -0400

    quitar validaciones anexo17

--- ModeloRegistro/views/dashboard/modelos/anexo17/Add.xaml.cs
+++ ModeloRegistro/views/dashboard/modelos/anexo17/Add.xaml.cs
@@ -24,21 +24,21 @@ namespace ModeloRegistro.views.dashboard.modelos.anexo17
         {
             var anexo = new model.Anexo17_e()
             {
-                nombre_apellidos=nombre_apellidos.Text,
-                ciudadano=ciudadano.Text,
-                lugar_nacimiento=lugar_nacimiento.Text,
-                fecha_nacimiento=fecha_nacimiento.SelectedDate?.ToString("yyyy-MM-dd"),
-                pasaporte=pasaporte.Text,
-                padre=padre.Text,
-                ciudadania_padre=ciudadania_padre.Text,
-                madre=madre.Text,
-                ciudadania_madre=ciudadania_madre.Text,
-                recidir_cuba=recidir_cuba.IsChecked.ToString(),
-                recidir_en_exterior=recidir_en_exterior.IsChecked.ToString(),
-                razones_personales=razones_personales.Text,
-                nombre_funcionario=nombre_funcionario.Text,
-                telefono=telefono.Text,
-                email=email.Text
+                nombre_apellidos=ValidarCampo(nombre_apellidos.Text),
+                ciudadano=ValidarCampo(ciudadano.Text),
+                lugar_nacimiento=ValidarCampo(lugar_nacimiento.Text),
+                fecha_nacimiento=ValidarCampo(fecha_nacimiento.SelectedDate?.ToString("yyyy-MM-dd")),
+                pasaporte=ValidarCampo(pasaporte.Text),
+                padre=ValidarCampo(padre.Text),
+                ciudadania_padre=ValidarCampo(ciudadania_padre.Text),
+                madre=ValidarCampo(madre.Text),
+                ciudadania_madre=ValidarCampo(ciudadania_madre.Text),
+                recidir_cuba=ValidarCampo(recidir_cuba.IsChecked.ToString()),
+                recidir_en_exterior=ValidarCampo(recidir_en_exterior.IsChecked.ToString()),
+                razones_personales=ValidarCampo(razones_personales.Text),
+                nombre_funcionario=ValidarCampo(nombre_funcionario.Text),
+                telefono=ValidarCampo(telefono.Text),
+                email=ValidarCampo(email.Text)
             };
 
             if(ValidarAnexo(anexo))
@@ -58,22 +58,31 @@ namespace ModeloRegistro.views.dashboard.modelos.anexo17
                 return false; // Retorna false si el objeto es nulo
 
             // Verificar cada campo de la entidad
-            return !string.IsNullOrWhiteSpace(anexo.nombre_apellidos)&&
-                   !string.IsNullOrWhiteSpace(anexo.ciudadano)&&
-                   !string.IsNullOrWhiteSpace(anexo.lugar_nacimiento)&&
-                   !string.IsNullOrWhiteSpace(anexo.fecha_nacimiento)&&
-                   !string.IsNullOrWhiteSpace(anexo.pasaporte)&&
-                   !string.IsNullOrWhiteSpace(anexo.padre)&&
-                   !string.IsNullOrWhiteSpace(anexo.ciudadania_padre)&&
-                   !string.IsNullOrWhiteSpace(anexo.madre)&&
-                   !string.IsNullOrWhiteSpace(anexo.ciudadania_madre)&&
-                   //!string.IsNullOrWhiteSpace(anexo.recidir_cuba)&&
-                   //!string.IsNullOrWhiteSpace(anexo.recidir_en_exterior)&&
-                   !string.IsNullOrWhiteSpace(anexo.razones_personales)&&
-                   !string.IsNullOrWhiteSpace(anexo.nombre_funcionario)&&
-                   !string.IsNullOrWhiteSpace(anexo.telefono)&&
-                   !string.IsNullOrWhiteSpace(anexo.email);
+            //return !string.IsNullOrWhiteSpace(anexo.nombre_apellidos)&&
+            //       !string.IsNullOrWhiteSpace(anexo.ciudadano)&&
+            //       !string.IsNullOrWhiteSpace(anexo.lugar_nacimiento)&&
+            //       !string.IsNullOrWhiteSpace(anexo.fecha_nacimiento)&&
+            //       !string.IsNullOrWhiteSpace(anexo.pasaporte)&&
+            //       !string.IsNullOrWhiteSpace(anexo.padre)&&
+            //       !string.IsNullOrWhiteSpace(anexo.ciudadania_padre)&&
+            //       !string.IsNullOrWhiteSpace(anexo.madre)&&
+            //       !string.IsNullOrWhiteSpace(anexo.ciudadania_madre)&&
+            //       //!string.IsNullOrWhiteSpace(anexo.recidir_cuba)&&
+            //       //!string.IsNullOrWhiteSpace(anexo.recidir_en_exterior)&&
+            //       !string.IsNullOrWhiteSpace(anexo.razones_personales)&&
+            //       !string.IsNullOrWhiteSpace(anexo.nombre_funcionario)&&
+            //       !string.IsNullOrWhiteSpace(anexo.telefono)&&
+            //       !string.IsNullOrWhiteSpace(anexo.email);
+
+            return true;
         }
 
+        private string ValidarCampo( string val )
+        {
+            string res = "";
+            if(!string.IsNullOrWhiteSpace(val))
+                res=val;
+            return res;
+        }
     }
 }
