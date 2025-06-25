1e1a35c4618f9dbae41089be37db244dd9325662
Roilan Lauzardo <125706938+rls1990@users.noreply.github.com>
2025-06-25 12:28:02 -0400

    quitar validacion anexo 21

--- ModeloRegistro/views/dashboard/modelos/anexo21/Add.xaml.cs
+++ ModeloRegistro/views/dashboard/modelos/anexo21/Add.xaml.cs
@@ -24,28 +24,28 @@ namespace ModeloRegistro.views.dashboard.modelos.anexo21
         {
             var anexo = new model.Anexo21_e()
             {
-                fecha_solicitud=fecha_solicitud.SelectedDate?.ToString("yyyy-MM-dd"),
-                nombre_padre=nombre_padre.Text,
-                ciudadania_padre=ciudadania_padre.Text,
-                estado_civil_padre=estado_civil_padre.Text,
-                minicipio_nacimineto_padre=minicipio_nacimineto_padre.Text,
-                provincia_nacimineto_padre=provincia_nacimineto_padre.Text,
-                fecha_nacimiento_padre=fecha_nacimiento_padre.SelectedDate?.ToString("yyyy-MM-dd"),
-                profecion_padre=profecion_padre.Text,
-                carnet_pasaporte_padre=carnet_pasaporte_padre.Text,
-                categoria_migratoria_padre=categoria_migratoria_padre.Text,
-                direccion_eu_padre=direccion_eu_padre.Text,
-                correo=correo.Text,
-                celular=celular.Text,
-                nombre_menor=nombre_menor.Text,
-                municipio_nacimiento_menor=municipio_nacimiento_menor.Text,
-                provincia_nacimiento_menor=provincia_nacimiento_menor.Text,
-                fecha_nacimiento_menor=fecha_nacimiento_menor.SelectedDate?.ToString("yyyy-MM-dd"),
-                registro_estado_nacimiento_menor=registro_estado_nacimiento_menor.Text,
-                tomo=tomo.Text,
-                folio=folio.Text,
-                tarjeta_menor=tarjeta_menor.Text,
-                direccion_menor_cuba=direccion_menor_cuba.Text
+                fecha_solicitud=ValidarCampo(fecha_solicitud.SelectedDate?.ToString("yyyy-MM-dd")),
+                nombre_padre=ValidarCampo(nombre_padre.Text),
+                ciudadania_padre=ValidarCampo(ciudadania_padre.Text),
+                estado_civil_padre=ValidarCampo(estado_civil_padre.Text),
+                minicipio_nacimineto_padre=ValidarCampo(minicipio_nacimineto_padre.Text),
+                provincia_nacimineto_padre=ValidarCampo(provincia_nacimineto_padre.Text),
+                fecha_nacimiento_padre=ValidarCampo(fecha_nacimiento_padre.SelectedDate?.ToString("yyyy-MM-dd")),
+                profecion_padre=ValidarCampo(profecion_padre.Text),
+                carnet_pasaporte_padre=ValidarCampo(carnet_pasaporte_padre.Text),
+                categoria_migratoria_padre=ValidarCampo(categoria_migratoria_padre.Text),
+                direccion_eu_padre=ValidarCampo(direccion_eu_padre.Text),
+                correo=ValidarCampo(correo.Text),
+                celular=ValidarCampo(celular.Text),
+                nombre_menor=ValidarCampo(nombre_menor.Text),
+                municipio_nacimiento_menor=ValidarCampo(municipio_nacimiento_menor.Text),
+                provincia_nacimiento_menor=ValidarCampo(provincia_nacimiento_menor.Text),
+                fecha_nacimiento_menor=ValidarCampo(fecha_nacimiento_menor.SelectedDate?.ToString("yyyy-MM-dd")),
+                registro_estado_nacimiento_menor=ValidarCampo(registro_estado_nacimiento_menor.Text),
+                tomo=ValidarCampo(tomo.Text),
+                folio=ValidarCampo(folio.Text),
+                tarjeta_menor=ValidarCampo(tarjeta_menor.Text),
+                direccion_menor_cuba=ValidarCampo(direccion_menor_cuba.Text)
             };
 
             if(ValidarAnexo(anexo))
@@ -68,35 +68,42 @@ namespace ModeloRegistro.views.dashboard.modelos.anexo21
             }
 
             // Verificar si cada campo es nulo o vacío
-            if(string.IsNullOrEmpty(model.fecha_solicitud)||
-                string.IsNullOrEmpty(model.nombre_padre)||
-                string.IsNullOrEmpty(model.ciudadania_padre)||
-                string.IsNullOrEmpty(model.estado_civil_padre)||
-                string.IsNullOrEmpty(model.minicipio_nacimineto_padre)||
-                string.IsNullOrEmpty(model.provincia_nacimineto_padre)||
-                string.IsNullOrEmpty(model.fecha_nacimiento_padre)||
-                string.IsNullOrEmpty(model.profecion_padre)||
-                string.IsNullOrEmpty(model.carnet_pasaporte_padre)||
-                string.IsNullOrEmpty(model.categoria_migratoria_padre)||
-                string.IsNullOrEmpty(model.direccion_eu_padre)||
-                string.IsNullOrEmpty(model.correo)||
-                string.IsNullOrEmpty(model.celular)||
-                string.IsNullOrEmpty(model.nombre_menor)||
-                string.IsNullOrEmpty(model.municipio_nacimiento_menor)||
-                string.IsNullOrEmpty(model.provincia_nacimiento_menor)||
-                string.IsNullOrEmpty(model.fecha_nacimiento_menor)||
-                string.IsNullOrEmpty(model.registro_estado_nacimiento_menor)||
-                string.IsNullOrEmpty(model.tomo)||
-                string.IsNullOrEmpty(model.folio)||
-                string.IsNullOrEmpty(model.tarjeta_menor)||
-                string.IsNullOrEmpty(model.direccion_menor_cuba))
-            {
-                return false; // Algún campo es nulo o vacío
-            }
+            //if(string.IsNullOrEmpty(model.fecha_solicitud)||
+            //    string.IsNullOrEmpty(model.nombre_padre)||
+            //    string.IsNullOrEmpty(model.ciudadania_padre)||
+            //    string.IsNullOrEmpty(model.estado_civil_padre)||
+            //    string.IsNullOrEmpty(model.minicipio_nacimineto_padre)||
+            //    string.IsNullOrEmpty(model.provincia_nacimineto_padre)||
+            //    string.IsNullOrEmpty(model.fecha_nacimiento_padre)||
+            //    string.IsNullOrEmpty(model.profecion_padre)||
+            //    string.IsNullOrEmpty(model.carnet_pasaporte_padre)||
+            //    string.IsNullOrEmpty(model.categoria_migratoria_padre)||
+            //    string.IsNullOrEmpty(model.direccion_eu_padre)||
+            //    string.IsNullOrEmpty(model.correo)||
+            //    string.IsNullOrEmpty(model.celular)||
+            //    string.IsNullOrEmpty(model.nombre_menor)||
+            //    string.IsNullOrEmpty(model.municipio_nacimiento_menor)||
+            //    string.IsNullOrEmpty(model.provincia_nacimiento_menor)||
+            //    string.IsNullOrEmpty(model.fecha_nacimiento_menor)||
+            //    string.IsNullOrEmpty(model.registro_estado_nacimiento_menor)||
+            //    string.IsNullOrEmpty(model.tomo)||
+            //    string.IsNullOrEmpty(model.folio)||
+            //    string.IsNullOrEmpty(model.tarjeta_menor)||
+            //    string.IsNullOrEmpty(model.direccion_menor_cuba))
+            //{
+            //    return false; // Algún campo es nulo o vacío
+            //}
 
             // Si todas las validaciones pasan
             return true; // La entidad Anexo21_e es válida
         }
 
+        private string ValidarCampo( string val )
+        {
+            string res = "";
+            if(!string.IsNullOrWhiteSpace(val))
+                res=val;
+            return res;
+        }
     }
 }
