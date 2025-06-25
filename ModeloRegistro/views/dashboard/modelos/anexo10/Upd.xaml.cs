8c0d7d6d859c9329cd35e6b7516ca0d96e2597b4
Roilan Lauzardo <125706938+rls1990@users.noreply.github.com>
2025-06-25 10:02:09 -0400

    quitar validacion anexo 11 y 12

--- ModeloRegistro/views/dashboard/modelos/anexo10/Upd.xaml.cs
+++ ModeloRegistro/views/dashboard/modelos/anexo10/Upd.xaml.cs
@@ -57,26 +57,26 @@ namespace ModeloRegistro.views.dashboard.modelos.anexo10
             var anexo = new model.Anexo10_e()
             {
                 id=entity.id,
-                ciudad=ciudad.Text,
-                pais=pais.Text,
-                solicito_certificado=solicito_certificado.Text,
-                de_mi=de_mi.Text,
-                nombre_apellidos=nombre_apellidos.Text,
-                fecha_nacimiento=fecha_nacimiento.SelectedDate?.ToString("yyyy-MM-dd"),
-                lugar_nacimiento=lugar_nacimiento.Text,
-                nombre_padres=nombre_padres.Text,
-                lugar_vivio_cuba=lugar_vivio_cuba.Text,
-                fecha_fallecimiento=fecha_fallecimiento.SelectedDate?.ToString("yyyy-MM-dd"),
-                otros_datos_interes=otros_datos_interes.Text,
-                nombre_apellidos_solicitante=nombre_apellidos_solicitante.Text,
-                doc_identidad=doc_identidad.Text,
-                pasaporte=pasaporte.Text,
-                direccion=direccion.Text,
-                telefono=telefono.Text,
-                email=email.Text,
-                legalizacion_minrex=legalizacion_minrex.Text,
-                legalizacion_embajada=legalizacion_embajada.Text,
-                fecha_de_solicitud=fecha_de_solicitud.SelectedDate?.ToString("yyyy-MM-dd")
+                ciudad=ValidarCampo(ciudad.Text),
+                pais=ValidarCampo(pais.Text),
+                solicito_certificado=ValidarCampo(solicito_certificado.Text),
+                de_mi=ValidarCampo(de_mi.Text),
+                nombre_apellidos=ValidarCampo(nombre_apellidos.Text),
+                fecha_nacimiento=ValidarCampo(fecha_nacimiento.SelectedDate?.ToString("yyyy-MM-dd")),
+                lugar_nacimiento=ValidarCampo(lugar_nacimiento.Text),
+                nombre_padres=ValidarCampo(nombre_padres.Text),
+                lugar_vivio_cuba=ValidarCampo(lugar_vivio_cuba.Text),
+                fecha_fallecimiento=ValidarCampo(fecha_fallecimiento.SelectedDate?.ToString("yyyy-MM-dd")),
+                otros_datos_interes=ValidarCampo(otros_datos_interes.Text),
+                nombre_apellidos_solicitante=ValidarCampo(nombre_apellidos_solicitante.Text),
+                doc_identidad=ValidarCampo(doc_identidad.Text),
+                pasaporte=ValidarCampo(pasaporte.Text),
+                direccion=ValidarCampo(direccion.Text),
+                telefono=ValidarCampo(telefono.Text),
+                email=ValidarCampo(email.Text),
+                legalizacion_minrex=ValidarCampo(legalizacion_minrex.Text),
+                legalizacion_embajada=ValidarCampo(legalizacion_embajada.Text),
+                fecha_de_solicitud=ValidarCampo(fecha_de_solicitud.SelectedDate?.ToString("yyyy-MM-dd"))
             };
 
 
@@ -94,26 +94,35 @@ namespace ModeloRegistro.views.dashboard.modelos.anexo10
         private bool ValidarAnexo( model.Anexo10_e anexo )
         {
             // Verificar si cada campo de la entidad no es un string vacío
-            return !string.IsNullOrWhiteSpace(anexo.ciudad)&&
-                   !string.IsNullOrWhiteSpace(anexo.pais)&&
-                   !string.IsNullOrWhiteSpace(anexo.solicito_certificado)&&
-                   !string.IsNullOrWhiteSpace(anexo.de_mi)&&
-                   !string.IsNullOrWhiteSpace(anexo.nombre_apellidos)&&
-                   !string.IsNullOrWhiteSpace(anexo.fecha_nacimiento)&&
-                   !string.IsNullOrWhiteSpace(anexo.lugar_nacimiento)&&
-                   !string.IsNullOrWhiteSpace(anexo.nombre_padres)&&
-                   !string.IsNullOrWhiteSpace(anexo.lugar_vivio_cuba)&&
-                   !string.IsNullOrWhiteSpace(anexo.fecha_fallecimiento)&&
-                   !string.IsNullOrWhiteSpace(anexo.otros_datos_interes)&&
-                   !string.IsNullOrWhiteSpace(anexo.nombre_apellidos_solicitante)&&
-                   !string.IsNullOrWhiteSpace(anexo.doc_identidad)&&
-                   !string.IsNullOrWhiteSpace(anexo.pasaporte)&&
-                   !string.IsNullOrWhiteSpace(anexo.direccion)&&
-                   !string.IsNullOrWhiteSpace(anexo.telefono)&&
-                   !string.IsNullOrWhiteSpace(anexo.email)&&
-                   !string.IsNullOrWhiteSpace(anexo.legalizacion_minrex)&&
-                   !string.IsNullOrWhiteSpace(anexo.legalizacion_embajada)&&
-                   !string.IsNullOrWhiteSpace(anexo.fecha_de_solicitud);
+            //return !string.IsNullOrWhiteSpace(anexo.ciudad)&&
+            //       !string.IsNullOrWhiteSpace(anexo.pais)&&
+            //       !string.IsNullOrWhiteSpace(anexo.solicito_certificado)&&
+            //       !string.IsNullOrWhiteSpace(anexo.de_mi)&&
+            //       !string.IsNullOrWhiteSpace(anexo.nombre_apellidos)&&
+            //       !string.IsNullOrWhiteSpace(anexo.fecha_nacimiento)&&
+            //       !string.IsNullOrWhiteSpace(anexo.lugar_nacimiento)&&
+            //       !string.IsNullOrWhiteSpace(anexo.nombre_padres)&&
+            //       !string.IsNullOrWhiteSpace(anexo.lugar_vivio_cuba)&&
+            //       !string.IsNullOrWhiteSpace(anexo.fecha_fallecimiento)&&
+            //       !string.IsNullOrWhiteSpace(anexo.otros_datos_interes)&&
+            //       !string.IsNullOrWhiteSpace(anexo.nombre_apellidos_solicitante)&&
+            //       !string.IsNullOrWhiteSpace(anexo.doc_identidad)&&
+            //       !string.IsNullOrWhiteSpace(anexo.pasaporte)&&
+            //       !string.IsNullOrWhiteSpace(anexo.direccion)&&
+            //       !string.IsNullOrWhiteSpace(anexo.telefono)&&
+            //       !string.IsNullOrWhiteSpace(anexo.email)&&
+            //       !string.IsNullOrWhiteSpace(anexo.legalizacion_minrex)&&
+            //       !string.IsNullOrWhiteSpace(anexo.legalizacion_embajada)&&
+            //       !string.IsNullOrWhiteSpace(anexo.fecha_de_solicitud);
+            return true;
+        }
+
+        private string ValidarCampo( string val )
+        {
+            string res = "";
+            if(!string.IsNullOrWhiteSpace(val))
+                res=val;
+            return res;
         }
     }
 }
