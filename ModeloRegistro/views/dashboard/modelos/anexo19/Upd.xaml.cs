782d537d442956b460ba2b3a165ca3e63a779a0f
Roilan Lauzardo <125706938+rls1990@users.noreply.github.com>
2025-06-25 11:50:45 -0400

    quitar validacion de anexo 19

--- ModeloRegistro/views/dashboard/modelos/anexo19/Upd.xaml.cs
+++ ModeloRegistro/views/dashboard/modelos/anexo19/Upd.xaml.cs
@@ -62,23 +62,23 @@ namespace ModeloRegistro.views.dashboard.modelos.anexo19
             var anexo = new model.Anexo19_e()
             {
                 id=entity.id,
-                nombre_apellidos=nombre_apellidos.Text,
-                ciudadano=ciudadano.Text,
-                lugar_nacimiento=lugar_nacimiento.Text,
-                fecha_nacimiento=fecha_nacimiento.SelectedDate?.ToString("yyyy-MM-dd"),
-                pasaporte=pasaporte.Text,
-                nombre_menor=nombre_menor.Text,
-                ciudadania_menor=ciudadania_menor.Text,
-                lugar_nacimineto_menor=lugar_nacimineto_menor.Text,
-                fecha_nacimiento_menor=fecha_nacimiento_menor.SelectedDate?.ToString("yyyy-MM-dd"),
-                pasaporte_menor=pasaporte_menor.Text,
-                ultima_direccion_pedres=ultima_direccion_pedres.Text,
-                recidir_cuba=recidir_cuba.IsChecked.ToString(),
-                recidir_en_exterior=recidir_en_exterior.IsChecked.ToString(),
-                razones_personales=razones_personales.Text,
-                nombre_funcionario=nombre_funcionario.Text,
-                telefono=telefono.Text,
-                email=email.Text
+                nombre_apellidos=ValidarCampo(nombre_apellidos.Text),
+                ciudadano=ValidarCampo(ciudadano.Text),
+                lugar_nacimiento=ValidarCampo(lugar_nacimiento.Text),
+                fecha_nacimiento=ValidarCampo(fecha_nacimiento.SelectedDate?.ToString("yyyy-MM-dd")),
+                pasaporte=ValidarCampo(pasaporte.Text),
+                nombre_menor=ValidarCampo(nombre_menor.Text),
+                ciudadania_menor=ValidarCampo(ciudadania_menor.Text),
+                lugar_nacimineto_menor=ValidarCampo(lugar_nacimineto_menor.Text),
+                fecha_nacimiento_menor=ValidarCampo(fecha_nacimiento_menor.SelectedDate?.ToString("yyyy-MM-dd")),
+                pasaporte_menor=ValidarCampo(pasaporte_menor.Text),
+                ultima_direccion_pedres=ValidarCampo(ultima_direccion_pedres.Text),
+                recidir_cuba=ValidarCampo(recidir_cuba.IsChecked.ToString()),
+                recidir_en_exterior=ValidarCampo(recidir_en_exterior.IsChecked.ToString()),
+                razones_personales=ValidarCampo(razones_personales.Text),
+                nombre_funcionario=ValidarCampo(nombre_funcionario.Text),
+                telefono=ValidarCampo(telefono.Text),
+                email=ValidarCampo(email.Text)
             };
 
             if(ValidarAnexo(anexo))
@@ -100,29 +100,37 @@ namespace ModeloRegistro.views.dashboard.modelos.anexo19
             }
 
             // Verificar si cada campo es nulo o vacío
-            if(string.IsNullOrEmpty(model.nombre_apellidos)||
-                string.IsNullOrEmpty(model.ciudadano)||
-                string.IsNullOrEmpty(model.lugar_nacimiento)||
-                string.IsNullOrEmpty(model.fecha_nacimiento)||
-                string.IsNullOrEmpty(model.pasaporte)||
-                string.IsNullOrEmpty(model.nombre_menor)||
-                string.IsNullOrEmpty(model.ciudadania_menor)||
-                string.IsNullOrEmpty(model.lugar_nacimineto_menor)||
-                string.IsNullOrEmpty(model.fecha_nacimiento_menor)||
-                string.IsNullOrEmpty(model.pasaporte_menor)||
-                string.IsNullOrEmpty(model.ultima_direccion_pedres)||
-                //string.IsNullOrEmpty(model.recidir_cuba)||
-                //string.IsNullOrEmpty(model.recidir_en_exterior)||
-                string.IsNullOrEmpty(model.razones_personales)||
-                string.IsNullOrEmpty(model.nombre_funcionario)||
-                string.IsNullOrEmpty(model.telefono)||
-                string.IsNullOrEmpty(model.email))
-            {
-                return false; // Algún campo es nulo o vacío
-            }
+            //if(string.IsNullOrEmpty(model.nombre_apellidos)||
+            //    string.IsNullOrEmpty(model.ciudadano)||
+            //    string.IsNullOrEmpty(model.lugar_nacimiento)||
+            //    string.IsNullOrEmpty(model.fecha_nacimiento)||
+            //    string.IsNullOrEmpty(model.pasaporte)||
+            //    string.IsNullOrEmpty(model.nombre_menor)||
+            //    string.IsNullOrEmpty(model.ciudadania_menor)||
+            //    string.IsNullOrEmpty(model.lugar_nacimineto_menor)||
+            //    string.IsNullOrEmpty(model.fecha_nacimiento_menor)||
+            //    string.IsNullOrEmpty(model.pasaporte_menor)||
+            //    string.IsNullOrEmpty(model.ultima_direccion_pedres)||
+            //    //string.IsNullOrEmpty(model.recidir_cuba)||
+            //    //string.IsNullOrEmpty(model.recidir_en_exterior)||
+            //    string.IsNullOrEmpty(model.razones_personales)||
+            //    string.IsNullOrEmpty(model.nombre_funcionario)||
+            //    string.IsNullOrEmpty(model.telefono)||
+            //    string.IsNullOrEmpty(model.email))
+            //{
+            //    return false; // Algún campo es nulo o vacío
+            //}
 
             // Si todas las validaciones pasan
             return true; // La entidad Anexo19_e es válida
+        }
+
+        private string ValidarCampo( string val )
+        {
+            string res = "";
+            if(!string.IsNullOrWhiteSpace(val))
+                res=val;
+            return res;
         }
     }
 }
