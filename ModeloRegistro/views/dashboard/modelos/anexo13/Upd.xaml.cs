1793c2ecccf367ffef7f7323b55180929ae1ecf0
Roilan Lauzardo <125706938+rls1990@users.noreply.github.com>
2025-06-25 10:42:51 -0400

    quitar validaciones anexo 13

--- ModeloRegistro/views/dashboard/modelos/anexo13/Upd.xaml.cs
+++ ModeloRegistro/views/dashboard/modelos/anexo13/Upd.xaml.cs
@@ -73,43 +73,43 @@ namespace ModeloRegistro.views.dashboard.modelos.anexo13
             var anexo = new model.Anexo13_e()
             {
                 id=entity.id,
-                fecha_solicitud=fecha_solicitud.SelectedDate?.ToString("yyyy-MM-dd"),
-                nombre_comparece=nombre_comparece.Text,
-                no_pasaporte_comparece=no_pasaporte_comparece.Text,
-                nombre_inscripcion=nombre_inscripcion.Text,
-                m=m.IsChecked.ToString(),
-                f=f.IsChecked.ToString(),
-                primer_apellido_inscripcion=primer_apellido_inscripcion.Text,
-                segundo_apellido_inscripcion=segundo_apellido_inscripcion.Text,
-                hora_nacimiento=hora_nacimiento.Text,
-                fecha_nacimineto=fecha_nacimineto.SelectedDate?.ToString("yyyy-MM-dd"),
-                referencia_territorio_registro_civil=referencia_territorio_registro_civil.Text,
-                tomo_referencia_registral=tomo_referencia_registral.Text,
-                folio_referencia_registral=folio_referencia_registral.Text,
-                nro_asiento=nro_asiento.Text,
-                lugar_nacimineto=lugar_nacimineto.Text,
-                nombre_padre=nombre_padre.Text,
-                primer_apellido_padre=primer_apellido_padre.Text,
-                segundo_apellido_padre=segundo_apellido_padre.Text,
-                ciudadania_padre=ciudadania_padre.Text,
-                natural_de_padre=natural_de_padre.Text,
-                nro_pasaporte_padre=nro_pasaporte_padre.Text,
-                domicilio_padre=domicilio_padre.Text,
-                nombre_madre=nombre_madre.Text,
-                primer_apellido_madre=primer_apellido_madre.Text,
-                segundo_apellido_madre=segundo_apellido_madre.Text,
-                ciudadania_madre=ciudadania_madre.Text,
-                natural_de_madre=natural_de_madre.Text,
-                nro_pasaporte_madre=nro_pasaporte_madre.Text,
-                domicilio_madre=domicilio_madre.Text,
-                nombre_abuelo_paterno=nombre_abuelo_paterno.Text,
-                nombre_abuela_paterna=nombre_abuela_paterna.Text,
-                nombre_abuelo_materno=nombre_abuelo_materno.Text,
-                nombre_abuela_materna=nombre_abuela_materna.Text,
-                nombre_apellido_soltera_madre=nombre_apellido_soltera_madre.Text,
-                nombre_inscrito_pais_nacimineto=nombre_inscrito_pais_nacimineto.Text,
-                inscripcion_practicada_en_virtud_de=inscripcion_practicada_en_virtud_de.Text,
-                observaciones=observaciones.Text
+                fecha_solicitud=ValidarCampo(fecha_solicitud.SelectedDate?.ToString("yyyy-MM-dd")),
+                nombre_comparece=ValidarCampo(nombre_comparece.Text),
+                no_pasaporte_comparece=ValidarCampo(no_pasaporte_comparece.Text),
+                nombre_inscripcion=ValidarCampo(nombre_inscripcion.Text),
+                m=ValidarCampo(m.IsChecked.ToString()),
+                f=ValidarCampo(f.IsChecked.ToString()),
+                primer_apellido_inscripcion=ValidarCampo(primer_apellido_inscripcion.Text),
+                segundo_apellido_inscripcion=ValidarCampo(segundo_apellido_inscripcion.Text),
+                hora_nacimiento=ValidarCampo(hora_nacimiento.Text),
+                fecha_nacimineto=ValidarCampo(fecha_nacimineto.SelectedDate?.ToString("yyyy-MM-dd")),
+                referencia_territorio_registro_civil=ValidarCampo(referencia_territorio_registro_civil.Text),
+                tomo_referencia_registral=ValidarCampo(tomo_referencia_registral.Text),
+                folio_referencia_registral=ValidarCampo(folio_referencia_registral.Text),
+                nro_asiento=ValidarCampo(nro_asiento.Text),
+                lugar_nacimineto=ValidarCampo(lugar_nacimineto.Text),
+                nombre_padre=ValidarCampo(nombre_padre.Text),
+                primer_apellido_padre=ValidarCampo(primer_apellido_padre.Text),
+                segundo_apellido_padre=ValidarCampo(segundo_apellido_padre.Text),
+                ciudadania_padre=ValidarCampo(ciudadania_padre.Text),
+                natural_de_padre=ValidarCampo(natural_de_padre.Text),
+                nro_pasaporte_padre=ValidarCampo(nro_pasaporte_padre.Text),
+                domicilio_padre=ValidarCampo(domicilio_padre.Text),
+                nombre_madre=ValidarCampo(nombre_madre.Text),
+                primer_apellido_madre=ValidarCampo(primer_apellido_madre.Text),
+                segundo_apellido_madre=ValidarCampo(segundo_apellido_madre.Text),
+                ciudadania_madre=ValidarCampo(ciudadania_madre.Text),
+                natural_de_madre=ValidarCampo(natural_de_madre.Text),
+                nro_pasaporte_madre=ValidarCampo(nro_pasaporte_madre.Text),
+                domicilio_madre=ValidarCampo(domicilio_madre.Text),
+                nombre_abuelo_paterno=ValidarCampo(nombre_abuelo_paterno.Text),
+                nombre_abuela_paterna=ValidarCampo(nombre_abuela_paterna.Text),
+                nombre_abuelo_materno=ValidarCampo(nombre_abuelo_materno.Text),
+                nombre_abuela_materna=ValidarCampo(nombre_abuela_materna.Text),
+                nombre_apellido_soltera_madre=ValidarCampo(nombre_apellido_soltera_madre.Text),
+                nombre_inscrito_pais_nacimineto=ValidarCampo(nombre_inscrito_pais_nacimineto.Text),
+                inscripcion_practicada_en_virtud_de=ValidarCampo(inscripcion_practicada_en_virtud_de.Text),
+                observaciones=ValidarCampo(observaciones.Text)
             };
 
             if(ValidarAnexo(anexo))
@@ -128,43 +128,53 @@ namespace ModeloRegistro.views.dashboard.modelos.anexo13
                 return false; // Retorna false si el objeto es nulo
 
             // Verificar cada campo de la entidad
-            return !string.IsNullOrWhiteSpace(anexo.fecha_solicitud)&&
-                   !string.IsNullOrWhiteSpace(anexo.nombre_comparece)&&
-                   !string.IsNullOrWhiteSpace(anexo.no_pasaporte_comparece)&&
-                   !string.IsNullOrWhiteSpace(anexo.nombre_inscripcion)&&
-                   //!string.IsNullOrWhiteSpace(anexo.m)&&
-                   //!string.IsNullOrWhiteSpace(anexo.f)&&
-                   !string.IsNullOrWhiteSpace(anexo.primer_apellido_inscripcion)&&
-                   !string.IsNullOrWhiteSpace(anexo.segundo_apellido_inscripcion)&&
-                   !string.IsNullOrWhiteSpace(anexo.hora_nacimiento)&&
-                   !string.IsNullOrWhiteSpace(anexo.fecha_nacimineto)&&
-                   !string.IsNullOrWhiteSpace(anexo.referencia_territorio_registro_civil)&&
-                   !string.IsNullOrWhiteSpace(anexo.tomo_referencia_registral)&&
-                   !string.IsNullOrWhiteSpace(anexo.folio_referencia_registral)&&
-                   !string.IsNullOrWhiteSpace(anexo.nro_asiento)&&
-                   !string.IsNullOrWhiteSpace(anexo.lugar_nacimineto)&&
-                   !string.IsNullOrWhiteSpace(anexo.nombre_padre)&&
-                   !string.IsNullOrWhiteSpace(anexo.primer_apellido_padre)&&
-                   !string.IsNullOrWhiteSpace(anexo.segundo_apellido_padre)&&
-                   !string.IsNullOrWhiteSpace(anexo.ciudadania_padre)&&
-                   !string.IsNullOrWhiteSpace(anexo.natural_de_padre)&&
-                   !string.IsNullOrWhiteSpace(anexo.nro_pasaporte_padre)&&
-                   !string.IsNullOrWhiteSpace(anexo.domicilio_padre)&&
-                   !string.IsNullOrWhiteSpace(anexo.nombre_madre)&&
-                   !string.IsNullOrWhiteSpace(anexo.primer_apellido_madre)&&
-                   !string.IsNullOrWhiteSpace(anexo.segundo_apellido_madre)&&
-                   !string.IsNullOrWhiteSpace(anexo.ciudadania_madre)&&
-                   !string.IsNullOrWhiteSpace(anexo.natural_de_madre)&&
-                   !string.IsNullOrWhiteSpace(anexo.nro_pasaporte_madre)&&
-                   !string.IsNullOrWhiteSpace(anexo.domicilio_madre)&&
-                   !string.IsNullOrWhiteSpace(anexo.nombre_abuelo_paterno)&&
-                   !string.IsNullOrWhiteSpace(anexo.nombre_abuela_paterna)&&
-                   !string.IsNullOrWhiteSpace(anexo.nombre_abuelo_materno)&&
-                   !string.IsNullOrWhiteSpace(anexo.nombre_abuela_materna)&&
-                   !string.IsNullOrWhiteSpace(anexo.nombre_apellido_soltera_madre)&&
-                   !string.IsNullOrWhiteSpace(anexo.nombre_inscrito_pais_nacimineto)&&
-                   !string.IsNullOrWhiteSpace(anexo.inscripcion_practicada_en_virtud_de)&&
-                   !string.IsNullOrWhiteSpace(anexo.observaciones);
+            //return !string.IsNullOrWhiteSpace(anexo.fecha_solicitud)&&
+            //       !string.IsNullOrWhiteSpace(anexo.nombre_comparece)&&
+            //       !string.IsNullOrWhiteSpace(anexo.no_pasaporte_comparece)&&
+            //       !string.IsNullOrWhiteSpace(anexo.nombre_inscripcion)&&
+            //       //!string.IsNullOrWhiteSpace(anexo.m)&&
+            //       //!string.IsNullOrWhiteSpace(anexo.f)&&
+            //       !string.IsNullOrWhiteSpace(anexo.primer_apellido_inscripcion)&&
+            //       !string.IsNullOrWhiteSpace(anexo.segundo_apellido_inscripcion)&&
+            //       !string.IsNullOrWhiteSpace(anexo.hora_nacimiento)&&
+            //       !string.IsNullOrWhiteSpace(anexo.fecha_nacimineto)&&
+            //       !string.IsNullOrWhiteSpace(anexo.referencia_territorio_registro_civil)&&
+            //       !string.IsNullOrWhiteSpace(anexo.tomo_referencia_registral)&&
+            //       !string.IsNullOrWhiteSpace(anexo.folio_referencia_registral)&&
+            //       !string.IsNullOrWhiteSpace(anexo.nro_asiento)&&
+            //       !string.IsNullOrWhiteSpace(anexo.lugar_nacimineto)&&
+            //       !string.IsNullOrWhiteSpace(anexo.nombre_padre)&&
+            //       !string.IsNullOrWhiteSpace(anexo.primer_apellido_padre)&&
+            //       !string.IsNullOrWhiteSpace(anexo.segundo_apellido_padre)&&
+            //       !string.IsNullOrWhiteSpace(anexo.ciudadania_padre)&&
+            //       !string.IsNullOrWhiteSpace(anexo.natural_de_padre)&&
+            //       !string.IsNullOrWhiteSpace(anexo.nro_pasaporte_padre)&&
+            //       !string.IsNullOrWhiteSpace(anexo.domicilio_padre)&&
+            //       !string.IsNullOrWhiteSpace(anexo.nombre_madre)&&
+            //       !string.IsNullOrWhiteSpace(anexo.primer_apellido_madre)&&
+            //       !string.IsNullOrWhiteSpace(anexo.segundo_apellido_madre)&&
+            //       !string.IsNullOrWhiteSpace(anexo.ciudadania_madre)&&
+            //       !string.IsNullOrWhiteSpace(anexo.natural_de_madre)&&
+            //       !string.IsNullOrWhiteSpace(anexo.nro_pasaporte_madre)&&
+            //       !string.IsNullOrWhiteSpace(anexo.domicilio_madre)&&
+            //       !string.IsNullOrWhiteSpace(anexo.nombre_abuelo_paterno)&&
+            //       !string.IsNullOrWhiteSpace(anexo.nombre_abuela_paterna)&&
+            //       !string.IsNullOrWhiteSpace(anexo.nombre_abuelo_materno)&&
+            //       !string.IsNullOrWhiteSpace(anexo.nombre_abuela_materna)&&
+            //       !string.IsNullOrWhiteSpace(anexo.nombre_apellido_soltera_madre)&&
+            //       !string.IsNullOrWhiteSpace(anexo.nombre_inscrito_pais_nacimineto)&&
+            //       !string.IsNullOrWhiteSpace(anexo.inscripcion_practicada_en_virtud_de)&&
+            //       !string.IsNullOrWhiteSpace(anexo.observaciones);
+
+            return true;
+        }
+
+        private string ValidarCampo( string val )
+        {
+            string res = "";
+            if(!string.IsNullOrWhiteSpace(val))
+                res=val;
+            return res;
         }
     }
 }
