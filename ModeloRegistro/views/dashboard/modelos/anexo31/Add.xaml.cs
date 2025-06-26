f534c0d1c0733a72a145f6f55adaaf446fd3ddb1
Roilan Lauzardo <125706938+rls1990@users.noreply.github.com>
2025-06-25 20:43:29 -0400

    quitar validacion anexo31

--- ModeloRegistro/views/dashboard/modelos/anexo31/Add.xaml.cs
+++ ModeloRegistro/views/dashboard/modelos/anexo31/Add.xaml.cs
@@ -24,38 +24,38 @@ namespace ModeloRegistro.views.dashboard.modelos.anexo31
         {
             var anexo31 = new model.Anexo31_e()
             {
-                fecha_solicitud=fecha_solicitud.SelectedDate?.ToString("yyyy-MM-dd"),
-                nombre_poderante=nombre_poderante.Text,
-                lugar_nacimiento_municipio_poderante=lugar_nacimiento_municipio_poderante.Text,
-                lugar_nacimiento_provincia_poderante=lugar_nacimiento_provincia_poderante.Text,
-                fecha_nacimineto_poderante=fecha_nacimineto_poderante.SelectedDate?.ToString("yyyy-MM-dd"),
-                registro_estado_civil_poderante=registro_estado_civil_poderante.Text,
-                tomo_poderante=tomo_poderante.Text,
-                folio_poderante=folio_poderante.Text,
-                estado_civil_poderante=estado_civil_poderante.Text,
-                ciudadania_poderante=ciudadania_poderante.Text,
-                ocupacion_poderante=ocupacion_poderante.Text,
-                direccion_particular_poderante=direccion_particular_poderante.Text,
-                carne_pasaporte_poderante=carne_pasaporte_poderante.Text,
-                nombre_apellido_padres_poderante=nombre_apellido_padres_poderante.Text,
-                condicion_migratoria_poderante=condicion_migratoria_poderante.Text,
-                correo_poderante=correo_poderante.Text,
-                celular_poderante=celular_poderante.Text,
-                nombre_apoderado=nombre_apoderado.Text,
-                lugar_nacimiento_municipio_apoderado=lugar_nacimiento_municipio_apoderado.Text,
-                lugar_nacimiento_provincia_apoderado=lugar_nacimiento_provincia_apoderado.Text,
-                fecha_nacimineto_apoderado=fecha_nacimineto_apoderado.SelectedDate?.ToString("yyyy-MM-dd"),
-                registro_estado_civil_apoderado=registro_estado_civil_apoderado.Text,
-                tomo_apoderado=tomo_apoderado.Text,
-                folio_apoderado=folio_apoderado.Text,
-                estado_civil_apoderado=estado_civil_apoderado.Text,
-                ciudadania_apoderado=ciudadania_apoderado.Text,
-                ocupacion_apoderado=ocupacion_apoderado.Text,
-                direccion_particular_apoderado=direccion_particular_apoderado.Text,
-                carne_pasaporte_apoderado=carne_pasaporte_apoderado.Text,
-                nombre_apellido_padres_apoderado=nombre_apellido_padres_apoderado.Text,
-                condicion_migratoria_apoderado=condicion_migratoria_apoderado.Text,
-                clausula=clausula.Text
+                fecha_solicitud=ValidarCampo(fecha_solicitud.SelectedDate?.ToString("yyyy-MM-dd")),
+                nombre_poderante=ValidarCampo(nombre_poderante.Text),
+                lugar_nacimiento_municipio_poderante=ValidarCampo(lugar_nacimiento_municipio_poderante.Text),
+                lugar_nacimiento_provincia_poderante=ValidarCampo(lugar_nacimiento_provincia_poderante.Text),
+                fecha_nacimineto_poderante=ValidarCampo(fecha_nacimineto_poderante.SelectedDate?.ToString("yyyy-MM-dd")),
+                registro_estado_civil_poderante=ValidarCampo(registro_estado_civil_poderante.Text),
+                tomo_poderante=ValidarCampo(tomo_poderante.Text),
+                folio_poderante=ValidarCampo(folio_poderante.Text),
+                estado_civil_poderante=ValidarCampo(estado_civil_poderante.Text),
+                ciudadania_poderante=ValidarCampo(ciudadania_poderante.Text),
+                ocupacion_poderante=ValidarCampo(ocupacion_poderante.Text),
+                direccion_particular_poderante=ValidarCampo(direccion_particular_poderante.Text),
+                carne_pasaporte_poderante=ValidarCampo(carne_pasaporte_poderante.Text),
+                nombre_apellido_padres_poderante=ValidarCampo(nombre_apellido_padres_poderante.Text),
+                condicion_migratoria_poderante=ValidarCampo(condicion_migratoria_poderante.Text),
+                correo_poderante=ValidarCampo(correo_poderante.Text),
+                celular_poderante=ValidarCampo(celular_poderante.Text),
+                nombre_apoderado=ValidarCampo(nombre_apoderado.Text),
+                lugar_nacimiento_municipio_apoderado=ValidarCampo(lugar_nacimiento_municipio_apoderado.Text),
+                lugar_nacimiento_provincia_apoderado=ValidarCampo(lugar_nacimiento_provincia_apoderado.Text),
+                fecha_nacimineto_apoderado=ValidarCampo(fecha_nacimineto_apoderado.SelectedDate?.ToString("yyyy-MM-dd")),
+                registro_estado_civil_apoderado=ValidarCampo(registro_estado_civil_apoderado.Text),
+                tomo_apoderado=ValidarCampo(tomo_apoderado.Text),
+                folio_apoderado=ValidarCampo(folio_apoderado.Text),
+                estado_civil_apoderado=ValidarCampo(estado_civil_apoderado.Text),
+                ciudadania_apoderado=ValidarCampo(ciudadania_apoderado.Text),
+                ocupacion_apoderado=ValidarCampo(ocupacion_apoderado.Text),
+                direccion_particular_apoderado=ValidarCampo(direccion_particular_apoderado.Text),
+                carne_pasaporte_apoderado=ValidarCampo(carne_pasaporte_apoderado.Text),
+                nombre_apellido_padres_apoderado=ValidarCampo(nombre_apellido_padres_apoderado.Text),
+                condicion_migratoria_apoderado=ValidarCampo(condicion_migratoria_apoderado.Text),
+                clausula=ValidarCampo(clausula.Text)
             };
 
             if(ValidarAnexo(anexo31))
@@ -77,45 +77,52 @@ namespace ModeloRegistro.views.dashboard.modelos.anexo31
             }
 
             // Verificar si cada campo es nulo o vacío
-            if(string.IsNullOrEmpty(model.fecha_solicitud)||
-                string.IsNullOrEmpty(model.nombre_poderante)||
-                string.IsNullOrEmpty(model.lugar_nacimiento_municipio_poderante)||
-                string.IsNullOrEmpty(model.lugar_nacimiento_provincia_poderante)||
-                string.IsNullOrEmpty(model.fecha_nacimineto_poderante)||
-                string.IsNullOrEmpty(model.registro_estado_civil_poderante)||
-                string.IsNullOrEmpty(model.tomo_poderante)||
-                string.IsNullOrEmpty(model.folio_poderante)||
-                string.IsNullOrEmpty(model.estado_civil_poderante)||
-                string.IsNullOrEmpty(model.ciudadania_poderante)||
-                string.IsNullOrEmpty(model.ocupacion_poderante)||
-                string.IsNullOrEmpty(model.direccion_particular_poderante)||
-                string.IsNullOrEmpty(model.carne_pasaporte_poderante)||
-                string.IsNullOrEmpty(model.nombre_apellido_padres_poderante)||
-                string.IsNullOrEmpty(model.condicion_migratoria_poderante)||
-                string.IsNullOrEmpty(model.correo_poderante)||
-                string.IsNullOrEmpty(model.celular_poderante)||
-                string.IsNullOrEmpty(model.nombre_apoderado)||
-                string.IsNullOrEmpty(model.lugar_nacimiento_municipio_apoderado)||
-                string.IsNullOrEmpty(model.lugar_nacimiento_provincia_apoderado)||
-                string.IsNullOrEmpty(model.fecha_nacimineto_apoderado)||
-                string.IsNullOrEmpty(model.registro_estado_civil_apoderado)||
-                string.IsNullOrEmpty(model.tomo_apoderado)||
-                string.IsNullOrEmpty(model.folio_apoderado)||
-                string.IsNullOrEmpty(model.estado_civil_apoderado)||
-                string.IsNullOrEmpty(model.ciudadania_apoderado)||
-                string.IsNullOrEmpty(model.ocupacion_apoderado)||
-                string.IsNullOrEmpty(model.direccion_particular_apoderado)||
-                string.IsNullOrEmpty(model.carne_pasaporte_apoderado)||
-                string.IsNullOrEmpty(model.nombre_apellido_padres_apoderado)||
-                string.IsNullOrEmpty(model.condicion_migratoria_apoderado)||
-                string.IsNullOrEmpty(model.clausula))
-            {
-                return false; // Algún campo es nulo o vacío
-            }
+            //if(string.IsNullOrEmpty(model.fecha_solicitud)||
+            //    string.IsNullOrEmpty(model.nombre_poderante)||
+            //    string.IsNullOrEmpty(model.lugar_nacimiento_municipio_poderante)||
+            //    string.IsNullOrEmpty(model.lugar_nacimiento_provincia_poderante)||
+            //    string.IsNullOrEmpty(model.fecha_nacimineto_poderante)||
+            //    string.IsNullOrEmpty(model.registro_estado_civil_poderante)||
+            //    string.IsNullOrEmpty(model.tomo_poderante)||
+            //    string.IsNullOrEmpty(model.folio_poderante)||
+            //    string.IsNullOrEmpty(model.estado_civil_poderante)||
+            //    string.IsNullOrEmpty(model.ciudadania_poderante)||
+            //    string.IsNullOrEmpty(model.ocupacion_poderante)||
+            //    string.IsNullOrEmpty(model.direccion_particular_poderante)||
+            //    string.IsNullOrEmpty(model.carne_pasaporte_poderante)||
+            //    string.IsNullOrEmpty(model.nombre_apellido_padres_poderante)||
+            //    string.IsNullOrEmpty(model.condicion_migratoria_poderante)||
+            //    string.IsNullOrEmpty(model.correo_poderante)||
+            //    string.IsNullOrEmpty(model.celular_poderante)||
+            //    string.IsNullOrEmpty(model.nombre_apoderado)||
+            //    string.IsNullOrEmpty(model.lugar_nacimiento_municipio_apoderado)||
+            //    string.IsNullOrEmpty(model.lugar_nacimiento_provincia_apoderado)||
+            //    string.IsNullOrEmpty(model.fecha_nacimineto_apoderado)||
+            //    string.IsNullOrEmpty(model.registro_estado_civil_apoderado)||
+            //    string.IsNullOrEmpty(model.tomo_apoderado)||
+            //    string.IsNullOrEmpty(model.folio_apoderado)||
+            //    string.IsNullOrEmpty(model.estado_civil_apoderado)||
+            //    string.IsNullOrEmpty(model.ciudadania_apoderado)||
+            //    string.IsNullOrEmpty(model.ocupacion_apoderado)||
+            //    string.IsNullOrEmpty(model.direccion_particular_apoderado)||
+            //    string.IsNullOrEmpty(model.carne_pasaporte_apoderado)||
+            //    string.IsNullOrEmpty(model.nombre_apellido_padres_apoderado)||
+            //    string.IsNullOrEmpty(model.condicion_migratoria_apoderado)||
+            //    string.IsNullOrEmpty(model.clausula))
+            //{
+            //    return false; // Algún campo es nulo o vacío
+            //}
 
             // Si todas las validaciones pasan
             return true; // La entidad Anexo31_e es válida
         }
 
+        private string ValidarCampo( string val )
+        {
+            string res = "";
+            if(!string.IsNullOrWhiteSpace(val))
+                res=val;
+            return res;
+        }
     }
 }
