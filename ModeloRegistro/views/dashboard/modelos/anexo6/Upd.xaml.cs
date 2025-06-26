51dc3d7acf66040ff776c13abf72981a43d7adba
Roilan Lauzardo <125706938+rls1990@users.noreply.github.com>
2025-06-25 20:51:33 -0400

    quitar validacion anexo6

--- ModeloRegistro/views/dashboard/modelos/anexo6/Upd.xaml.cs
+++ ModeloRegistro/views/dashboard/modelos/anexo6/Upd.xaml.cs
@@ -55,19 +55,19 @@ namespace ModeloRegistro.views.dashboard.modelos.anexo6
             var anexo6 = new model.Anexo6_e()
             {
                 id=entity.id,
-                ciudad=ciudad.Text,
-                pais=pais.Text,
-                nombre_apellido_hombre=nombre_apellido_hombre.Text,
-                nombre_apellido_mujer=nombre_apellido_mujer.Text,
-                tribunal_dicto_sentencia=tribunal_dicto_sentencia.Text,
-                nombre_secretario_actuante=nombre_secretario_actuante.Text,
-                fecha_dicto_sentencia=fecha_dicto_sentencia.SelectedDate?.ToString("yyyy-MM-dd"),
-                fecha_firmeza_sentencia=fecha_firmeza_sentencia.SelectedDate?.ToString("yyyy-MM-dd"),
-                no_sentencia=no_sentencia.Text,
-                no_radicacion=no_radicacion.Text,
-                legalizacion_minred=legalizacion_minred.Text,
-                legalizacion_embajada=legalizacion_embajada.Text,
-                fecha_solicitud=fecha_solicitud.SelectedDate?.ToString("yyyy-MM-dd")
+                ciudad=ValidarCampo(ciudad.Text),
+                pais=ValidarCampo(pais.Text),
+                nombre_apellido_hombre=ValidarCampo(nombre_apellido_hombre.Text),
+                nombre_apellido_mujer=ValidarCampo(nombre_apellido_mujer.Text),
+                tribunal_dicto_sentencia=ValidarCampo(tribunal_dicto_sentencia.Text),
+                nombre_secretario_actuante=ValidarCampo(nombre_secretario_actuante.Text),
+                fecha_dicto_sentencia=ValidarCampo(fecha_dicto_sentencia.SelectedDate?.ToString("yyyy-MM-dd")),
+                fecha_firmeza_sentencia=ValidarCampo(fecha_firmeza_sentencia.SelectedDate?.ToString("yyyy-MM-dd")),
+                no_sentencia=ValidarCampo(no_sentencia.Text),
+                no_radicacion=ValidarCampo(no_radicacion.Text),
+                legalizacion_minred=ValidarCampo(legalizacion_minred.Text),
+                legalizacion_embajada=ValidarCampo(legalizacion_embajada.Text),
+                fecha_solicitud=ValidarCampo(fecha_solicitud.SelectedDate?.ToString("yyyy-MM-dd"))
             };
 
             if(ValidarAnexo(anexo6))
@@ -89,25 +89,32 @@ namespace ModeloRegistro.views.dashboard.modelos.anexo6
             }
 
             // Verificar si cada campo es nulo o vacío
-            if(string.IsNullOrEmpty(model.ciudad)||
-                string.IsNullOrEmpty(model.pais)||
-                string.IsNullOrEmpty(model.nombre_apellido_hombre)||
-                string.IsNullOrEmpty(model.nombre_apellido_mujer)||
-                string.IsNullOrEmpty(model.tribunal_dicto_sentencia)||
-                string.IsNullOrEmpty(model.nombre_secretario_actuante)||
-                string.IsNullOrEmpty(model.fecha_dicto_sentencia)||
-                string.IsNullOrEmpty(model.fecha_firmeza_sentencia)||
-                string.IsNullOrEmpty(model.no_sentencia)||
-                string.IsNullOrEmpty(model.no_radicacion)||
-                string.IsNullOrEmpty(model.legalizacion_minred)||
-                string.IsNullOrEmpty(model.legalizacion_embajada)||
-                string.IsNullOrEmpty(model.fecha_solicitud))
-            {
-                return false; // Algún campo es nulo o vacío
-            }
+            //if(string.IsNullOrEmpty(model.ciudad)||
+            //    string.IsNullOrEmpty(model.pais)||
+            //    string.IsNullOrEmpty(model.nombre_apellido_hombre)||
+            //    string.IsNullOrEmpty(model.nombre_apellido_mujer)||
+            //    string.IsNullOrEmpty(model.tribunal_dicto_sentencia)||
+            //    string.IsNullOrEmpty(model.nombre_secretario_actuante)||
+            //    string.IsNullOrEmpty(model.fecha_dicto_sentencia)||
+            //    string.IsNullOrEmpty(model.fecha_firmeza_sentencia)||
+            //    string.IsNullOrEmpty(model.no_sentencia)||
+            //    string.IsNullOrEmpty(model.no_radicacion)||
+            //    string.IsNullOrEmpty(model.legalizacion_minred)||
+            //    string.IsNullOrEmpty(model.legalizacion_embajada)||
+            //    string.IsNullOrEmpty(model.fecha_solicitud))
+            //{
+            //    return false; // Algún campo es nulo o vacío
+            //}
 
             // Si todas las validaciones pasan
             return true; // La entidad Anexo6_e es válida
+        }
+        private string ValidarCampo( string val )
+        {
+            string res = "";
+            if(!string.IsNullOrWhiteSpace(val))
+                res=val;
+            return res;
         }
     }
 }
