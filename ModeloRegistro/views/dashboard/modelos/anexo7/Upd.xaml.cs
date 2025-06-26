6d849c67553402422ab73ef9d4592440c3ab8e8e
Roilan Lauzardo <125706938+rls1990@users.noreply.github.com>
2025-06-25 21:02:51 -0400

    quitar validacion anexo7

--- ModeloRegistro/views/dashboard/modelos/anexo7/Upd.xaml.cs
+++ ModeloRegistro/views/dashboard/modelos/anexo7/Upd.xaml.cs
@@ -57,20 +57,20 @@ namespace ModeloRegistro.views.dashboard.modelos.anexo7
             var anexo7 = new model.Anexo7_e()
             {
                 id=entity.id,
-                ciudad=ciudad.Text,
-                pais=pais.Text,
-                nombre_apellido=nombre_apellido.Text,
-                fecha_defuncion=fecha_defuncion.SelectedDate?.ToString("yyyy-MM-dd"),
-                municipio_fallecimineto=municipio_fallecimineto.Text,
-                provincia_fallecimineto=provincia_fallecimineto.Text,
-                donde_fue_velado=donde_fue_velado.Text,
-                municipio_funeraria=municipio_funeraria.Text,
-                provincia_funeraria=provincia_funeraria.Text,
-                tomo=tomo.Text,
-                folio=folio.Text,
-                legalizacion_minrex=legalizacion_minrex.Text,
-                legalizacion_embajada=legalizacion_embajada.Text,
-                fecha_de_solicitud=fecha_de_solicitud.SelectedDate?.ToString("yyyy-MM-dd")
+                ciudad=ValidarCampo(ciudad.Text),
+                pais=ValidarCampo(pais.Text),
+                nombre_apellido=ValidarCampo(nombre_apellido.Text),
+                fecha_defuncion=ValidarCampo(fecha_defuncion.SelectedDate?.ToString("yyyy-MM-dd")),
+                municipio_fallecimineto=ValidarCampo(municipio_fallecimineto.Text),
+                provincia_fallecimineto=ValidarCampo(provincia_fallecimineto.Text),
+                donde_fue_velado=ValidarCampo(donde_fue_velado.Text),
+                municipio_funeraria=ValidarCampo(municipio_funeraria.Text),
+                provincia_funeraria=ValidarCampo(provincia_funeraria.Text),
+                tomo=ValidarCampo(tomo.Text),
+                folio=ValidarCampo(folio.Text),
+                legalizacion_minrex=ValidarCampo(legalizacion_minrex.Text),
+                legalizacion_embajada=ValidarCampo(legalizacion_embajada.Text),
+                fecha_de_solicitud=ValidarCampo(fecha_de_solicitud.SelectedDate?.ToString("yyyy-MM-dd"))
             };
 
             if(ValidarAnexo(anexo7))
@@ -92,26 +92,33 @@ namespace ModeloRegistro.views.dashboard.modelos.anexo7
             }
 
             // Verificar si cada campo es nulo o vacío
-            if(string.IsNullOrEmpty(model.ciudad)||
-                string.IsNullOrEmpty(model.pais)||
-                string.IsNullOrEmpty(model.nombre_apellido)||
-                string.IsNullOrEmpty(model.fecha_defuncion)||
-                string.IsNullOrEmpty(model.municipio_fallecimineto)||
-                string.IsNullOrEmpty(model.provincia_fallecimineto)||
-                string.IsNullOrEmpty(model.donde_fue_velado)||
-                string.IsNullOrEmpty(model.municipio_funeraria)||
-                string.IsNullOrEmpty(model.provincia_funeraria)||
-                string.IsNullOrEmpty(model.tomo)||
-                string.IsNullOrEmpty(model.folio)||
-                string.IsNullOrEmpty(model.legalizacion_minrex)||
-                string.IsNullOrEmpty(model.legalizacion_embajada)||
-                string.IsNullOrEmpty(model.fecha_de_solicitud))
-            {
-                return false; // Algún campo es nulo o vacío
-            }
+            //if(string.IsNullOrEmpty(model.ciudad)||
+            //    string.IsNullOrEmpty(model.pais)||
+            //    string.IsNullOrEmpty(model.nombre_apellido)||
+            //    string.IsNullOrEmpty(model.fecha_defuncion)||
+            //    string.IsNullOrEmpty(model.municipio_fallecimineto)||
+            //    string.IsNullOrEmpty(model.provincia_fallecimineto)||
+            //    string.IsNullOrEmpty(model.donde_fue_velado)||
+            //    string.IsNullOrEmpty(model.municipio_funeraria)||
+            //    string.IsNullOrEmpty(model.provincia_funeraria)||
+            //    string.IsNullOrEmpty(model.tomo)||
+            //    string.IsNullOrEmpty(model.folio)||
+            //    string.IsNullOrEmpty(model.legalizacion_minrex)||
+            //    string.IsNullOrEmpty(model.legalizacion_embajada)||
+            //    string.IsNullOrEmpty(model.fecha_de_solicitud))
+            //{
+            //    return false; // Algún campo es nulo o vacío
+            //}
 
             // Si todas las validaciones pasan
             return true; // La entidad Anexo7_e es válida
+        }
+        private string ValidarCampo( string val )
+        {
+            string res = "";
+            if(!string.IsNullOrWhiteSpace(val))
+                res=val;
+            return res;
         }
     }
 }
