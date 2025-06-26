4f7890814da5222c0c2633127d4c9bff9483bf1d
Roilan Lauzardo <125706938+rls1990@users.noreply.github.com>
2025-06-25 20:49:10 -0400

    quitar validacion anexo5

--- ModeloRegistro/views/dashboard/modelos/anexo5/Upd.xaml.cs
+++ ModeloRegistro/views/dashboard/modelos/anexo5/Upd.xaml.cs
@@ -60,22 +60,22 @@ namespace ModeloRegistro.views.dashboard.modelos.anexo5
             var anexo5 = new model.Anexo5_e()
             {
                 id=entity.id,
-                ciudad=ciudad.Text,
-                pais=pais.Text,
-                nombre_apellido_hombre=nombre_apellido_hombre.Text,
-                nombre_apellido_mujer=nombre_apellido_mujer.Text,
-                fecha_matrimonio=fecha_matrimonio.SelectedDate?.ToString("yyyy-MM-dd"),
-                ciudad_matrimonio=ciudad_matrimonio.Text,
-                provincia_matrimonio=provincia_matrimonio.Text,
-                pais_matrimonio=pais_matrimonio.Text,
-                registro=registro.Text,
-                municipio_registro=municipio_registro.Text,
-                provincia_registro=provincia_registro.Text,
-                tomo=tomo.Text,
-                folio=folio.Text,
-                legalizacion_minred=legalizacion_minred.Text,
-                legalizacion_embajada=legalizacion_embajada.Text,
-                fecha_solicitud=fecha_solicitud.SelectedDate?.ToString("yyyy-MM-dd")
+                ciudad=ValidarCampo(ciudad.Text),
+                pais=ValidarCampo(pais.Text),
+                nombre_apellido_hombre=ValidarCampo(nombre_apellido_hombre.Text),
+                nombre_apellido_mujer=ValidarCampo(nombre_apellido_mujer.Text),
+                fecha_matrimonio=ValidarCampo(fecha_matrimonio.SelectedDate?.ToString("yyyy-MM-dd")),
+                ciudad_matrimonio=ValidarCampo(ciudad_matrimonio.Text),
+                provincia_matrimonio=ValidarCampo(provincia_matrimonio.Text),
+                pais_matrimonio=ValidarCampo(pais_matrimonio.Text),
+                registro=ValidarCampo(registro.Text),
+                municipio_registro=ValidarCampo(municipio_registro.Text),
+                provincia_registro=ValidarCampo(provincia_registro.Text),
+                tomo=ValidarCampo(tomo.Text),
+                folio=ValidarCampo(folio.Text),
+                legalizacion_minred=ValidarCampo(legalizacion_minred.Text),
+                legalizacion_embajada=ValidarCampo(legalizacion_embajada.Text),
+                fecha_solicitud=ValidarCampo(fecha_solicitud.SelectedDate?.ToString("yyyy-MM-dd"))
             };
 
             if(ValidarAnexo(anexo5))
@@ -97,28 +97,35 @@ namespace ModeloRegistro.views.dashboard.modelos.anexo5
             }
 
             // Verificar si cada campo es nulo o vacío
-            if(string.IsNullOrEmpty(model.ciudad)||
-                string.IsNullOrEmpty(model.pais)||
-                string.IsNullOrEmpty(model.nombre_apellido_hombre)||
-                string.IsNullOrEmpty(model.nombre_apellido_mujer)||
-                string.IsNullOrEmpty(model.fecha_matrimonio)||
-                string.IsNullOrEmpty(model.ciudad_matrimonio)||
-                string.IsNullOrEmpty(model.provincia_matrimonio)||
-                string.IsNullOrEmpty(model.pais_matrimonio)||
-                string.IsNullOrEmpty(model.registro)||
-                string.IsNullOrEmpty(model.municipio_registro)||
-                string.IsNullOrEmpty(model.provincia_registro)||
-                string.IsNullOrEmpty(model.tomo)||
-                string.IsNullOrEmpty(model.folio)||
-                string.IsNullOrEmpty(model.legalizacion_minred)||
-                string.IsNullOrEmpty(model.legalizacion_embajada)||
-                string.IsNullOrEmpty(model.fecha_solicitud))
-            {
-                return false; // Algún campo es nulo o vacío
-            }
+            //if(string.IsNullOrEmpty(model.ciudad)||
+            //    string.IsNullOrEmpty(model.pais)||
+            //    string.IsNullOrEmpty(model.nombre_apellido_hombre)||
+            //    string.IsNullOrEmpty(model.nombre_apellido_mujer)||
+            //    string.IsNullOrEmpty(model.fecha_matrimonio)||
+            //    string.IsNullOrEmpty(model.ciudad_matrimonio)||
+            //    string.IsNullOrEmpty(model.provincia_matrimonio)||
+            //    string.IsNullOrEmpty(model.pais_matrimonio)||
+            //    string.IsNullOrEmpty(model.registro)||
+            //    string.IsNullOrEmpty(model.municipio_registro)||
+            //    string.IsNullOrEmpty(model.provincia_registro)||
+            //    string.IsNullOrEmpty(model.tomo)||
+            //    string.IsNullOrEmpty(model.folio)||
+            //    string.IsNullOrEmpty(model.legalizacion_minred)||
+            //    string.IsNullOrEmpty(model.legalizacion_embajada)||
+            //    string.IsNullOrEmpty(model.fecha_solicitud))
+            //{
+            //    return false; // Algún campo es nulo o vacío
+            //}
 
             // Si todas las validaciones pasan
             return true; // La entidad Anexo5_e es válida
+        }
+        private string ValidarCampo( string val )
+        {
+            string res = "";
+            if(!string.IsNullOrWhiteSpace(val))
+                res=val;
+            return res;
         }
     }
 }
